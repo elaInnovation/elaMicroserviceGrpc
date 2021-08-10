f264b9b039f5b2495cbbe64d45f60d2941b2a858
matbuster <abet.mathieu@gmail.com>
2021-08-10 11:56:34 +0200

    update the proto authentication for all the new functionnalities

--- Constant/ElaGrpcConstants.cs
+++ Constant/ElaGrpcConstants.cs
@@ -51,7 +51,7 @@ namespace elaMicroservicesGrpc.Constant
         /// ///////////////////////////////////////////////////////
         // Proto version constant definition
         public static readonly uint VERSION_PROTO_MAJOR = 1;
-        public static readonly uint VERSION_PROTO_MINOR = 3;
+        public static readonly uint VERSION_PROTO_MINOR = 4;
         public static readonly uint VERSION_PROTO_RELEASE = 0;
         //
 
