d20fbd92e6c1cc342217dd5aff222f17617ee45e
Paul Fournier <paul.fournier@ELA.local>
2024-05-29 11:43:54 +0200

    update for tagset sticker

--- Constant/ElaGrpcConstants.cs
+++ Constant/ElaGrpcConstants.cs
@@ -52,7 +52,7 @@ namespace elaMicroservicesGrpc.Constant
         /// ///////////////////////////////////////////////////////
         // Bluetooth Master definition
         public static readonly int PORT_BLUETOOTH_MASTER_REMOTE_API = 50054;
-        public static readonly String DEFAULT_BLUETOOTH_MASTER_BASE_NAME = "Bluetooth Master Service";        
+        public static readonly String DEFAULT_BLUETOOTH_MASTER_BASE_NAME = "Bluetooth Master Service";
         /// ///////////////////////////////////////////////////////
         // EPE Master definition
         public static readonly int PORT_EPE_REMOTE_API = 50055;
